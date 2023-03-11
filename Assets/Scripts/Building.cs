374dad35e9695916c6af8bf5b4c127fcf227798e
Saeed Jannati <m.jannati@emofid.com>
2023-03-11 17:45:08 +0330

    Simple placeing indicator

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -10,7 +10,10 @@ public class Building : MonoBehaviour
     [SerializeField] private LayerMask obstacleLayer;
     [SerializeField] private Collider _collider;
     [SerializeField] private int _obstaclesTriggering = 0;
-    [SerializeField]  private List<Material> _materials;
+    [SerializeField] private GameObject _borderObject;
+    [SerializeField] private Material _borderObjectMat;
+    [SerializeField] private Color _placeableColour;
+    [SerializeField] private Color _notPlaceableColor;
     #endregion
 
     #region Unity Actions
@@ -20,6 +23,7 @@ public class Building : MonoBehaviour
         if ((obstacleLayer.value & 1 << other.gameObject.layer) == 0)
             return;
         _obstaclesTriggering++;
+        OnTriggerCountChange();
     }
 
     private void OnTriggerExit(Collider other)
@@ -27,16 +31,29 @@ public class Building : MonoBehaviour
         if ((obstacleLayer.value & 1 << other.gameObject.layer) == 0)
             return;
         _obstaclesTriggering--;
+        OnTriggerCountChange();
     }
+
 
     #endregion
 
     #region Methods
+    void OnTriggerCountChange()
+    {
+        var colour = _obstaclesTriggering > 0 ? _notPlaceableColor : _placeableColour;
+        SetMatColour(colour);
+    }
 
+    void SetMatColour(Color colour)
+    {
+        _borderObjectMat.color = colour;
+    }
     public void OnCreate()
     {
         _obstaclesTriggering = 0;
         _collider.isTrigger = true;
+        SetMatColour(_placeableColour);
+        _borderObject.SetActive(true);
     }
 
     public bool TryPlace()
@@ -44,14 +61,17 @@ public class Building : MonoBehaviour
         if (_obstaclesTriggering != 0)
             return false;
         _collider.isTrigger = false;
+        
+        _borderObject.SetActive(false);
         return true;
     }
 
-    [Button]
-    void ExtractMaterials()
-    {
-        _materials = GetComponentsInChildren<Renderer>().Select(i=>i.sharedMaterial).ToList();
-    }
+
+    #endregion
+
+    #region Serialisble classes
+
+    
 
     #endregion
 }
