9e99fbab537d157890f6ff3a01c55e9756776f5c
Saeed Jannati <m.jannati@emofid.com>
2023-03-11 16:43:56 +0330

    adding zenject and basic building placement system

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private LayerMask obstacleLayer;
+    [SerializeField] private Collider _collider;
+    [SerializeField] private int _obstaclesTriggering = 0;
+
+    #endregion
+
+    #region Unity Actions
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((obstacleLayer.value & 1 << other.gameObject.layer) == 0)
+            return;
+        _obstaclesTriggering++;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if ((obstacleLayer.value & 1 << other.gameObject.layer) == 0)
+            return;
+        _obstaclesTriggering--;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public void OnCreate()
+    {
+        _obstaclesTriggering = 0;
+        _collider.isTrigger = true;
+    }
+
+    public bool TryPlace()
+    {
+        if (_obstaclesTriggering != 0)
+            return false;
+        _collider.isTrigger = false;
+        return true;
+    }
+
+    #endregion
+}
