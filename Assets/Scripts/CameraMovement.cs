0363fe686f77a1b4565df5b15391ec98fa8e6453
Saeed Jannati <msaeedjannati72@gmail.com>
2023-03-15 17:28:11 +0330

    Camera pinch zoom and drag move

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,63 @@
+using System;
+using Lean.Touch;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    #region Fields
+
+    private Vector3 xAxis;
+    private Vector3 yAxis;
+    [SerializeField] private float moveSpeed = 3.0f;
+    [SerializeField] private Collider _cameraBound;
+    [SerializeField] private Transform _camTransform;
+    [SerializeField] private CameraSwipeMove _cameraSwipeMove;
+    [SerializeField] private LeanPinchCamera _cameraPinch;
+
+    #endregion
+
+    #region Unity actions
+
+    private void Start()
+    {
+        RegisterToEvents();
+        CalcAxis();
+    }
+
+    private void OnDestroy()
+    {
+        UnregisterFromEvents();
+    }
+
+    #endregion
+
+    #region Methods
+
+    void RegisterToEvents()
+    {
+        _cameraSwipeMove.onSwipe += OnSwipe;
+    }
+
+    void UnregisterFromEvents()
+    {
+        _cameraSwipeMove.onSwipe -= OnSwipe;
+    }
+
+    void CalcAxis()
+    {
+        var twoRoot = Mathf.Sqrt(2.0f);
+        xAxis = twoRoot / 2 * new Vector3(1, 0, -1);
+        yAxis = twoRoot / 2 * new Vector3(1, 0, 1);
+    }
+
+    async void OnSwipe(Vector3 deltaMove)
+    {
+        if (Input.touchCount > 1)
+            return;
+        var deltaPos = Time.deltaTime * moveSpeed * (deltaMove.x * xAxis + deltaMove.y * yAxis);
+
+        _camTransform.position += deltaPos;
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/CameraSwipeMove.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CameraSwipeMove : MonoBehaviour
+{
+    #region Fields
+
+    private Camera _mainCam;
+    [SerializeField] private Vector3 _lastPos;
+
+    #endregion
+
+    #region Properties
+
+    public Action<Vector3> onSwipe { get; set; } = default;
+
+    #endregion
+
+    #region Unity actions
+
+    private void Start()
+    {
+        _mainCam = Camera.main;
+    }
+
+    #endregion
+
+    #region Methots
+
+    public void OnBeginDrag(BaseEventData data)
+    {
+        var mousePos = data.currentInputModule.input.mousePosition;
+        _lastPos = mousePos;
+    }
+
+    public void OnDrag(BaseEventData data)
+    {
+        var mousePos = data.currentInputModule.input.mousePosition;
+        var deltaPos =(Vector3) mousePos - _lastPos;
+        _lastPos = mousePos;
+        onSwipe?.Invoke(deltaPos);
+    }
+
+    public void OnDragEnd(BaseEventData data)
+    {
+    }
+
+    Vector3 CalcPointerPosition(BaseEventData data)
+    {
+        var mousePos = data.currentInputModule.input.mousePosition;
+        var pos = _mainCam.ScreenToWorldPoint(mousePos);
+        pos.z = 0.0f;
+        return pos;
+    }
+
+    #endregion
+}
