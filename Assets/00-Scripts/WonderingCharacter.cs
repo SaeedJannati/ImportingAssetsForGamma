d879dd5f419f511091518554376cd275352312dc
Saeed Jannati <m.jannati@emofid.com>
2023-03-18 14:00:01 +0330

    Adding basic state machine for characters

--- Assets/00-Scripts/WonderingCharacter.cs
+++ Assets/00-Scripts/WonderingCharacter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
@@ -13,8 +14,10 @@ public class WonderingCharacter : MonoBehaviour
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] private Animator _animator;
 
-    private int _idleHash = Animator.StringToHash("Idle");
-    private int _walkHash = Animator.StringToHash("Walk");
+    private readonly int _idleHash = Animator.StringToHash("Idle");
+    private readonly int _walkHash = Animator.StringToHash("Walk");
+    private readonly int _gatherHash = Animator.StringToHash("Gathering");
+    private readonly int _miningHash = Animator.StringToHash("Mining");
 
     [SerializeField] private float _minDistance;
     [SerializeField] private float _maxDistance;
@@ -22,6 +25,11 @@ public class WonderingCharacter : MonoBehaviour
     [SerializeField] private float _minWaitTime;
     [SerializeField] private float _maxWaitTime;
     [SerializeField] private float _moveSpeed;
+
+    [SerializeField] private GameObject _pickAxe;
+
+    [SerializeField] private float _gatherChance = .1f;
+    [SerializeField] private float _mineChance = .1f;
 
     private Vector3 _targetPoint;
 
@@ -37,6 +45,7 @@ public class WonderingCharacter : MonoBehaviour
     {
         _agent.speed = _moveSpeed;
         _transform = transform;
+        _pickAxe.SetActive(false);
     }
 
     private void Start()
@@ -51,7 +60,6 @@ public class WonderingCharacter : MonoBehaviour
     private async void FindTarget()
     {
         _targetPoint = await GetRandomDestPoint();
-        CreateDestPoint(_targetPoint);
         _agent.SetDestination(_targetPoint);
         _animator.SetTrigger(_walkHash);
         if (_walkRoutine != default)
@@ -100,17 +108,95 @@ public class WonderingCharacter : MonoBehaviour
             yield return null;
         }
 
+        OnReachedDest();
+    }
+
+    async Task Idle(float period = -1.0f)
+    {
         _animator.SetTrigger(_idleHash);
-        yield return new WaitForSeconds(GetRandomWaitTime());
+        if (period < 0)
+            period = GetRandomWaitTime();
+        await Task.Delay((int)(period * 1000));
+    }
+
+    async void OnReachedDest()
+    {
+        var nextState = GetNextState();
+        await DoAction(nextState);
         FindTarget();
     }
 
-    void CreateDestPoint(Vector3 pos)
+    void CreateSphere(Vector3 pos)
     {
         var obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         obj.transform.position = pos;
         obj.GetComponent<Renderer>().sharedMaterial.color = Color.red;
         obj.transform.localScale = Vector3.one * .2f;
+    }
+
+    [Button]
+    async Task Gather()
+    {
+        _animator.SetTrigger(_gatherHash);
+        await Task.Delay(2000);
+    }
+
+    [Button]
+    async Task Mine()
+    {
+        var times = 3;
+        _pickAxe.SetActive(true);
+        _animator.SetTrigger(_miningHash);
+        await Task.Delay(times * 1000);
+        _pickAxe.SetActive(false);
+        _animator.SetTrigger(_idleHash);
+        await Task.Delay(1000);
+        await Gather();
+    }
+
+    public enum CharacterState
+    {
+        Idle,
+        Walk,
+        Gather,
+        Mine,
+    }
+
+    async Task DoAction(CharacterState state)
+    {
+        switch (state)
+        {
+            case CharacterState.Gather:
+                await Gather();
+                break;
+            case CharacterState.Mine:
+                await Mine();
+                break;
+            case CharacterState.Idle:
+                await Idle();
+                break;
+        }
+    }
+
+    private CharacterState GetNextState()
+    {
+        if (GoGather())
+            return CharacterState.Gather;
+        if (GoMine())
+            return CharacterState.Mine;
+        return CharacterState.Idle;
+    }
+
+    private bool GoGather()
+    {
+        var randomNum = Random.Range(0.0f, 1.0f);
+        return !(randomNum > _gatherChance);
+    }
+
+    private bool GoMine()
+    {
+        var randomNum = Random.Range(0.0f, 1.0f);
+        return !(randomNum > _mineChance);
     }
 
     #endregion
